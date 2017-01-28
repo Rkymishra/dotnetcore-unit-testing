453bafff4dbf8dabcf440c874659872b4408b683
= <=>
2017-01-28 19:29:20 +0545

    added chapter 1

---
+++ unit-test-chap-1/src/Fibonacci.Services/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace FibonacciCalculation
+{
+    public class Fibonacci
+    {
+        public int Fib(int n)
+        {
+           if(n==0) {
+            return 0;
+            }
+            else if(n <= 2){
+                return 1;
+            }
+            return Fib(n-1) + Fib (n-2);
+        }
+    }
+
+}
---
+++ unit-test-chap-1/tests/Fibonacci.Tests/FibonacciUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Xunit;
+using  FibonacciCalculation;
+
+namespace Fibonaci.UnitTest
+{
+    public class FibonacciUnitTest
+    {
+        [Fact]
+        public void Given_0_Return_0()
+        {
+            //arrange
+            Fibonacci fibo = new Fibonacci();
+            int i = 0;
+            //act
+            int result = fibo.Fib(i);
+            //assert
+            Assert.Equal(result,0);
+        }
+        [Fact]
+        public void Given_1_Return_1()
+        {
+            //arrange
+            Fibonacci fibo = new Fibonacci();
+            int i = 1;
+            //act
+            int result = fibo.Fib(i);
+            //assert
+            Assert.Equal(result,1);
+        }
+
+        [Fact]
+public void Fib_Given2_Return1()
+{
+    //arrange
+    Fibonacci fibo = new Fibonacci();
+    int i = 2;
+    //act
+    int result = fibo.Fib(i);
+    //assert
+    Assert.Equal(1, result);
+}
+
+[Fact]
+public void Fib_Given3_Return2()
+{
+    //arrange
+    Fibonacci fibo = new Fibonacci();
+    int i = 3;
+    //act
+    int result = fibo.Fib(i);
+    //assert
+    Assert.Equal(2, result);
+}
+
+[Fact]
+public void Fib_Given4_Return3()
+{
+    //arrange
+    Fibonacci fibo = new Fibonacci();
+    int i = 4;
+    //act
+    int result = fibo.Fib(i);
+    //assert
+    Assert.Equal(3, result);
+}
+    }
+}
